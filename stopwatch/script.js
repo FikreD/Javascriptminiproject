efa02e2b3e84fce0ef13373870e528a691b58659
FikreD <desalegnfikre76@gmail.com>
2023-05-17 15:47:34 +0300

    first commit

---
+++ stopwatch/script.js
@@ -0,0 +1,57 @@
+
+let timer = document.getElementById("timer");
+let startBtn = document.getElementById("startBtn");
+let stopBtn = document.getElementById("stopBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+let interval;
+
+function startTimer() {
+    interval = setInterval(() => {
+        millisecond += 10;
+        if (millisecond == 1000) {
+            millisecond = 0;
+            second++;
+        }
+        if (second == 60) {
+            second = 0;
+            minute++;
+        }
+        if (minute == 60) {
+            minute = 0;
+            hour++;
+        }
+        displayTimer();
+    }, 10);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    hour = 0;
+    minute = 0;
+    second = 0;
+    millisecond = 0;
+    displayTimer();
+}
+
+function displayTimer() {
+    let h = hour < 10 ? "0" + hour : hour;
+    let m = minute < 10 ? "0" + minute : minute;
+    let s = second < 10 ? "0" + second : second;
+    let ms = millisecond < 10 ? "00" + millisecond : millisecond < 100 ? "0" + millisecond : millisecond;
+    timer.textContent = `${h}:${m}:${s}:${ms}`;
+}
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+
